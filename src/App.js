031cd83e8e5064f400e2fbbd37e6c7270cf61b27
eshumphries <eshumphries@outlook.com>
2024-09-25 17:14:54 -0400

    Fixing splash page bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import logo from './KomischeKatzenLogo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <img src="KomischeKatzen.png"/>
+          <img src={logo} alt="Logo"/>
         </div>
       </header>
     </div>
