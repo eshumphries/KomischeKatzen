260e4b4eaec76138250b4d2a4319e695733c7501
eshumphries <eshumphries@outlook.com>
2024-09-27 09:32:53 -0400

    Adjusting screen size for logo to fit screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import logo from './KomischeKatzenLogo.png';
+import { useMediaQuery } from 'react-responsive';
+
+const AdjustForMobile = () => {
+  const isMobile = useMediaQuery({ query: '(max-width: 600)' });
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>
+          <AdjustForMobile/>
           <img src={logo} alt="Logo"/>
         </div>
       </header>
